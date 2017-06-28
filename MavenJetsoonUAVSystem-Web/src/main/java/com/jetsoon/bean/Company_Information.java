1835b9c6549fb246c3ec7d4e43318f2e114e87b5
nipengge <nipengge@3743e648-8aec-ca44-9e46-2b950041a2dd>
2017-06-28 07:18:15 +0000

    初始化提交

---
+++ MavenJetsoonUAVSystem-Web/src/main/java/com/jetsoon/bean/Company_Information.java
@@ -0,0 +1,71 @@
+package com.jetsoon.bean;
+
+/**
+ * 企业信息表
+ * @author nipengge
+ * 作者:吕国朋
+ * 时间:2017-2-6 上午11:04:10
+ */
+public class Company_Information {
+	
+	private int eiId;
+	
+	private String companyName;
+	
+	private String companyAddress;
+	
+	private String companytel;
+	
+	private String subordinateIndustry;
+	
+	public String getCompanytel() {
+		return companytel;
+	}
+
+	public void setCompanytel(String companytel) {
+		this.companytel = companytel;
+	}
+
+	public int getEiId() {
+		return eiId;
+	}
+
+	public void setEiId(int eiId) {
+		this.eiId = eiId;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getCompanyAddress() {
+		return companyAddress;
+	}
+
+	public void setCompanyAddress(String companyAddress) {
+		this.companyAddress = companyAddress;
+	}
+
+	public String getSubordinateIndustry() {
+		return subordinateIndustry;
+	}
+
+	public void setSubordinateIndustry(String subordinateIndustry) {
+		this.subordinateIndustry = subordinateIndustry;
+	}
+
+	@Override
+	public String toString() {
+		return "Company_Information [companyAddress=" + companyAddress
+				+ ", companyName=" + companyName + ", companytel=" + companytel
+				+ ", eiId=" + eiId + ", subordinateIndustry="
+				+ subordinateIndustry + "]";
+	}
+	
+	
+
+}
